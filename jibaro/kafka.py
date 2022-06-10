e026e21cd6e4835a4c28cf8380c13d51686ffd5b
Yuri Niitsuma <ignitzhjfk@gmail.com>
2022-06-09 23:16:01 -0300

    :sparkles: Improve Jibaro

--- jibaro/kafka.py
+++ jibaro/kafka.py
@@ -5,12 +5,27 @@ def kafka_to_raw(spark, database, bootstrap_servers, topic):
 
     # TODO: Limit batch size in 1 million rows
     # spark.streaming.backpressure.enabled=true
+    # [Optional] Tweak to balance data processing parallelism vs. task scheduling overhead (Default: 200ms)
+    spark.conf.set("spark.streaming.blockInterval", "200")
+    # Prevent data loss on driver recovery
+    spark.conf.set("spark.streaming.receiver.writeAheadLog.enable", "true")
+    spark.conf.set("spark.streaming.backpressure.enabled", "true")
+    # [Optional] Reduce min rate of PID-based backpressure implementation (Default: 100)
+    spark.conf.set("spark.streaming.backpressure.pid.minRate", "10")
+    # [Spark 1.x]: Workaround for missing initial rate (Default: not set)
+    spark.conf.set("spark.streaming.receiver.maxRate", "100")
+    # [Spark 1.x]: Corresponding max rate setting for Direct Kafka Streaming (Default: not set)
+    spark.conf.set("spark.streaming.kafka.maxRatePerPartition", "100")
+    # [Spark 2.x]: Initial rate before backpressure kicks in (Default: not set)
+    spark.conf.set("spark.streaming.backpressure.initialRate", "30")
+
+
     df = (
         spark.readStream.format('kafka')
         .option('kafka.bootstrap.servers', bootstrap_servers)
         .option('subscribe', topic)
         .option('startingOffsets', 'earliest')
-        .option('maxOffsetsPerTrigger', 1)
+        .option('maxOffsetsPerTrigger', 100)
         .load()
     )
 
