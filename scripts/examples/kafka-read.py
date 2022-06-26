e48a519e31f635cd6c4a48990a309d4f223c6f57
Yuri Niitsuma <ignitzhjfk@gmail.com>
2022-06-26 02:00:35 -0300

    :construction: availableNow example in spark streaming

--- scripts/examples/kafka-read.py
+++ scripts/examples/kafka-read.py
@@ -4,23 +4,27 @@ from pyspark.sql import SparkSession
 spark = SparkSession.builder.appName("Spark Streaming Delta").getOrCreate()
 
 topic = sys.argv[1]
-bootstrap_servers = 'broker:29092'
+bootstrap_servers = 'localhost:9092'
 
 df = (
     spark.readStream.format('kafka')
     .option('kafka.bootstrap.servers', bootstrap_servers)
     .option('subscribe', topic)
     .option('startingOffsets', 'earliest')
+    # Will work only with Spark 3.3 with
+    .option('maxOffsetsPerTrigger', 100000)
     .load()
 )
 
-# df.writeStream.trigger(once=True).format("console").start().awaitTermination()
+# availableNow only work with Spark 3.3.x or later
+df.writeStream.trigger(availableNow=True).format(
+    "console").start().awaitTermination()
 
-path = f'datalake-raw/data/{topic}'
+# path = f'datalake-raw/data/{topic}'
 
-df.writeStream.trigger(once=True).format("delta").start(
-    path=f's3a://{path}',
-    checkpointLocation=f's3a://spark-control/_checkpoints/{path}'
-).awaitTermination()
+# df.writeStream.trigger(once=True).format("delta").start(
+#     path=f's3a://{path}',
+#     checkpointLocation=f's3a://spark-control/_checkpoints/{path}'
+# ).awaitTermination()
 
-spark.stop()
+# spark.stop()
